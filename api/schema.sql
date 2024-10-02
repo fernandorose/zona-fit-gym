551189128c215cd0d615f5aa09d9c6b08d0b0c9a
Fernando Rodriguez <ferrodriguezsegura@gmail.com>
2024-10-01 20:55:59 -0600

    Change some values from all tables

--- api/schema.sql
+++ api/schema.sql
@@ -1,19 +1,27 @@
 CREATE TABLE clients (
-  id UUID PRIMARY KEY,
+  id VARCHAR(30) PRIMARY KEY,
   name VARCHAR(100) NOT NULL,
   email VARCHAR(100) UNIQUE NOT NULL
 );
 
 CREATE TABLE memberships(
-  id UUID PRIMARY KEY,
+  id VARCHAR(30) PRIMARY KEY,
   type VARCHAR(50) NOT NULL,
   price DECIMAL(8, 2) NOT NULL
 );
 
 CREATE TABLE client_membership(
-  client_id UUID REFERENCES clients(id),
-  membership_id UUID REFERENCES memberships(id),
+  client_id VARCHAR REFERENCES clients(id),
+  membership_id VARCHAR REFERENCES memberships(id),
   start_date DATE NOT NULL,
   end_date DATE NOT NULL,
   PRIMARY KEY (client_id, membership_id)
+);
+
+CREATE TABLE products(
+  id VARCHAR(30) PRIMARY KEY,
+  name VARCHAR(100) NOT NULL,
+  purchase_price DECIMAL(8, 2) NOT NULL,
+  public_price DECIMAL(8, 2) NOT NULL,
+  quantity INT NOT NULL
 );
