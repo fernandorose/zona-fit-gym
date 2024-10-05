49bccf0f0474f0aff29d9413bd47bcd25a693e64
Fernando Rodriguez <ferrodriguezsegura@gmail.com>
2024-10-05 15:23:46 -0600

    Add global styles and font import to the main

--- admin/src/main.tsx
+++ admin/src/main.tsx
@@ -1,5 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
+// Supports weights 200-800
+import "@fontsource-variable/bricolage-grotesque";
+import "./styles/globals.css";
+
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
