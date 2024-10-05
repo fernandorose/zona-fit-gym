dbfeb59ef5ad797926481a9a5e8b386f586e1e03
Fernando Rodriguez <ferrodriguezsegura@gmail.com>
2024-10-05 11:32:00 -0600

    Add "on delete cascade" in client_membership table

--- api/schema.sql
+++ api/schema.sql
@@ -4,21 +4,21 @@ CREATE TABLE clients (
   email VARCHAR(100) UNIQUE NOT NULL
 );
 
-CREATE TABLE memberships(
+CREATE TABLE memberships (
   id VARCHAR(30) PRIMARY KEY,
   type VARCHAR(50) NOT NULL,
   price DECIMAL(8, 2) NOT NULL
 );
 
-CREATE TABLE client_membership(
-  client_id VARCHAR REFERENCES clients(id),
-  membership_id VARCHAR REFERENCES memberships(id),
+CREATE TABLE client_membership (
+  client_id VARCHAR(30) REFERENCES clients(id) ON DELETE CASCADE,
+  membership_id VARCHAR(30) REFERENCES memberships(id) ON DELETE CASCADE,
   start_date DATE NOT NULL,
   end_date DATE NOT NULL,
   PRIMARY KEY (client_id, membership_id)
 );
 
-CREATE TABLE products(
+CREATE TABLE products (
   id VARCHAR(30) PRIMARY KEY,
   name VARCHAR(100) NOT NULL,
   purchase_price DECIMAL(8, 2) NOT NULL,
